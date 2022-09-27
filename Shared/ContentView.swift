8a0c08ace76fcb802bdd319eb37564dd33f7bbcb
Henry <henry0523ho.3@gmail.com>
2022-09-28 02:35:50 +0800

    test

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading){
+            Group{
+                Text("News")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Text("September 28")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.gray)
+                Divider()
+                    
+            }
+            
+            Text("Top Stories")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.red)
+                .padding(.top)
+            Group{
+                Image("P1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:.infinity)
+                Text("Miami Heralo")
+                    .font(.caption)
+                Text("Hurricane lan now a fierce Category 3 as storm's projected path shifts")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                Text("7m ago")
+                    .font(.footnote)
+                    .foregroundColor(Color.gray)
+                    .padding(.vertical)
+                    
+            }
+        }
+        .padding(.all, 9.0)
+        .frame(width: .infinity,
+            height: .infinity
+            )
+        
     }
 }
 

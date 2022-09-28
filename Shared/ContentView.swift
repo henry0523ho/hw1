39c3dfe314e49ad360ada01a8ea78e348c9d7cb8
Henry <henry0523ho.3@gmail.com>
2022-09-28 15:34:11 +0800

    update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,50 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-            Group{
-                Text("News")
+        ZStack{
+            VStack(alignment: .leading){
+                Group{
+                    Text("News")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    Text("September 28")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.gray)
+                    Divider()
+                        
+                }
+                Group{
+                    Text("Top Stories")
                     .font(.largeTitle)
                     .fontWeight(.black)
-                Text("September 28")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(Color.gray)
-                Divider()
-                    
+                    .foregroundColor(Color.red)
+                    .padding(.top)
+                    Image("P1")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width:.infinity)
+                    Text("Miami Heralo")
+                        .font(.headline)
+                    Text("Hurricane lan now a fierce Category 3 as storm's projected path shifts")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.leading)
+                    Text("7m ago")
+                        .font(.footnote)
+                        .foregroundColor(Color.gray)
+                        .padding(.vertical)
+                }
+                HStack{
+                    Image("P2").resizable()
+                        .scaledToFill()
+                        .frame(width: .infinity/2, alignment: .top)
+                    Image("P3").resizable()
+                        .scaledToFill()
+                        .frame(width: .infinity/2, alignment: .top)
+                }
             }
+            .padding(.all, 9.0)
+            .frame(width: .infinity,
+                height: .infinity)
             Group{
-                Text("Top Stories")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(Color.red)
-                .padding(.top)
-                Image("P1")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:.infinity)
-                Text("Miami Heralo")
-                    .font(.caption)
-                Text("Hurricane lan now a fierce Category 3 as storm's projected path shifts")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
-                Text("7m ago")
-                    .font(.footnote)
-                    .foregroundColor(Color.gray)
-                    .padding(.vertical)
-            }
-            HStack{
-                Image("P2").resizable()
-                    .scaledToFit()
-                Image("P3").resizable()
-                    .scaledToFit()
-            }
+                HStack(alignment: .top){
+                    VStack{
+                        Text("hello")
+                            .font(.largeTitle)
+                    }
+                    VStack{
+                        Text("hello")
+                    }
+                    VStack{
+                        Text("hello")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.green)
+                    }
+                }
+                .padding(.top, 100.0)
+                .position(y:100)
+                
+            }.frame(width: .infinity, height: 50, alignment: .bottom)
+            
         }
-        .padding(.all, 9.0)
-        .frame(width: .infinity,
-            height: .infinity
-            )
+        
         
     }
 }
